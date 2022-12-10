f03a657af5b6e94100b2f5994184cb20a52c0e50
Kcfreshz <kckc176@gmail.com>
2022-12-10 00:18:46 -0800

    task9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,18 +7,16 @@
  */
 int main(void)
 {
-	int num;
+	int num = '0';
 
-	for (num = 0; num <= 9; num++)
+	while (num <= '9')
 	{
-		putchar((num % 10) + '0');
-		if (num == 9)
-			continue;
-		putchar(',');
-		putchar(' ');
+	putchar(num);
+	if (num != '9')
+	{
+	putchar(',');
+	putchar(' ');
 	}
-
 	putchar('\n');
-
 	return (0);
 }
